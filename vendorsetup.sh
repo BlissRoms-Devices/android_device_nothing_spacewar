3efaf53d1f42c03b47b267d43f3f13153c6a560d
AyanC1 <choudhury.ayan13@gmail.com>
2024-08-11 14:33:32 +0530

    vendorsetup.sh: extract firmware images on the fly

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,34 +2,6 @@
 export GLOBAL_THINLTO=false
 
 
-#rm -rf hardware/qcom-caf/sm8350/audio
+git lfs clone https://gitlab.com/StudioKeys/vendor_nothing_Spacewar_radio vendor/nothing/Spacewar/radio
 
-#rm -rf hardware/qcom-caf/sm8350/display
-
-#rm -rf hardware/qcom-caf/sm8350/media
-
-#rm -rf hardware/qcom-caf/common
-
-#rm -rf vendor/qcom/opensource/commonsys-intf/display
-
-#rm -rf vendor/qcom/opensource/commonsys/display
-
-#rm -rf vendor/qcom/opensource/display
-
-#rm -rf vendor/qcom/opensource/interfaces
-
-#git clone https://github.com/SuperiorOS/android_hardware_qcom_media.git -b thirteen-caf-sm8350 hardware/qcom-caf/sm8350/media
-
-#git clone https://github.com/SuperiorOS/android_hardware_qcom_display.git -b thirteen-caf-sm8350 hardware/qcom-caf/sm8350/display
-
-#git clone https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b thirteen-caf-sm8350 hardware/qcom-caf/sm8350/audio
-
-#git clone https://github.com/LineageOS/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
-
-#git clone https://github.com/SuperiorOS/android_vendor_qcom_opensource_display-commonsys-intf.git vendor/qcom/opensource/commonsys-intf/display
-
-#git clone https://github.com/SuperiorOS/android_vendor_qcom_opensource_commonsys_display.git vendor/qcom/opensource/commonsys/display
-
-#git clone https://github.com/SuperiorOS/android_vendor_qcom_opensource_display.git vendor/qcom/opensource/display
-
-#git clone https://github.com/SuperiorOS/android_vendor_qcom_opensource_interfaces.git vendor/qcom/opensource/interfaces
+bash extract-files.sh vendor/nothing/Spacewar/radio --only-firmware
