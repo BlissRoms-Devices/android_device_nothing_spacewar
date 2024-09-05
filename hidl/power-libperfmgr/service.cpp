609b15be4a3d61b544b1d910231828e2bf7a5e67
Matt Buckley <mattbuckley@google.com>
2024-09-05 04:20:55 -0400

    Refactoring PowerHAL to allow for better testing
    - Templated multiple classes, including singletons, to allow for mocking
      without incurring vtable overhead
    - Changed singletons to use raw pointers for test simplicity and
      efficiency, as there is no reason to keep shared_ptr for immortal
      classes
    - Disabled "using namespace" warning because it was harmful
    - Moved a number of common "using" definitions to a central file
    - Moved AppHintDesc into its own file to resolve circular dependency
    
    Test: libadpf_test
    Bug: 315894228
    Change-Id: Ia8e662ccb8b590b3e4b0ac6487a588418e2c9e55
    
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- hidl/power-libperfmgr/service.cpp
+++ hidl/power-libperfmgr/service.cpp
@@ -32,14 +32,13 @@
 using aidl::google::hardware::power::impl::pixel::Power;
 using aidl::google::hardware::power::impl::pixel::PowerExt;
 using aidl::google::hardware::power::impl::pixel::PowerHintMonitor;
-using aidl::google::hardware::power::impl::pixel::PowerSessionManager;
 using ::android::perfmgr::HintManager;
 
 constexpr std::string_view kPowerHalInitProp("vendor.powerhal.init");
 
 int main() {
     // Parse config but do not start the looper
-    std::shared_ptr<HintManager> hm = HintManager::GetInstance();
+    HintManager *hm = HintManager::GetInstance();
     if (!hm) {
         LOG(FATAL) << "HintManager Init failed";
     }
